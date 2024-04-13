6197d6693683f8a012856a70ea2153a6c69801b0
nisargaa20 <125184228+nisargaa20@users.noreply.github.com>
2024-04-13 10:00:06 +0530

    day

---
+++ Days in Month/day.py
@@ -0,0 +1,30 @@
+def leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def days_in_month(year, month):
+  
+    days_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+ 
+    if leap_year(year) and month == 2:
+        return 29
+    else:
+        return days_list[month - 1]
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+
+
+days = days_in_month(year, month)
+
+
+print(f"The number of days in month {month} of year {year} is: {days}")
